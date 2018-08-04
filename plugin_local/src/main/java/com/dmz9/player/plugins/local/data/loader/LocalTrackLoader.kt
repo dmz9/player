ff313c59fc15fdc3a67b3026f5ed6c38dff7f743
dmz9 <dmz9@yandex.ru>
2018-08-04 17:43:00 +0300

    refactoring: moved into plugin_local

--- plugin_local/src/main/java/com/dmz9/player/plugins/local/data/loader/LocalTrackLoader.kt
+++ plugin_local/src/main/java/com/dmz9/player/plugins/local/data/loader/LocalTrackLoader.kt
@@ -1,15 +1,16 @@
-package com.dmz9.player.data.loader
+package com.dmz9.player.plugins.local.data.loader
 
 import com.dmz9.player.domain.ITrackLoader
 import com.dmz9.player.domain.entity.Album
 import com.dmz9.player.domain.entity.Criteria
 import com.dmz9.player.domain.entity.Track
 import io.reactivex.Single
+import io.reactivex.Single.fromCallable
 import javax.inject.Inject
 
-class LocalTrackLoader @Inject constructor():ITrackLoader {
+class LocalTrackLoader @Inject constructor() : ITrackLoader {
     override fun next(criteria: Criteria): Single<out List<Track>> {
-        return Single.fromCallable{
+        return fromCallable {
             listOf(Track(name = "123", album = Album("Some album name")))
         }
     }
