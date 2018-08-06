a6276005518b3eebc1fa8d1a6f75028a6ca232b5
dmz9 <dmz9@yandex.ru>
2018-08-06 18:26:11 +0300

    optimize: withPool

--- plugin_local/src/main/java/com/dmz9/player/plugins/local/data/loader/LocalTrackLoader.kt
+++ plugin_local/src/main/java/com/dmz9/player/plugins/local/data/loader/LocalTrackLoader.kt
@@ -5,31 +5,34 @@ import android.provider.MediaStore
 import com.dmz9.player.domain.ITrackLoader
 import com.dmz9.player.domain.entity.Criteria
 import com.dmz9.player.domain.entity.Track
+import com.dmz9.player.shared.SchedulersPool
+import com.dmz9.player.shared.withPool
 import io.reactivex.Single
 import io.reactivex.Single.fromCallable
 import javax.inject.Inject
 
 class LocalTrackLoader @Inject constructor(
-    private val context: Context
+	private val context: Context,
+	private val pool: SchedulersPool
 ) : ITrackLoader {
-    override fun next(criteria: Criteria): Single<out List<Track>> {
-        return fromCallable {
-            val parser = CriteriaParser.from(criteria)
-            val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
-            val cursor = context.contentResolver.query(
-                uri,
-                parser.getProjection(),
-                parser.getSelection(),
-                parser.getSelectionArgs(),
-                parser.getSort()
-            )
-            val mapper = TrackMapper(cursor, parser.getProjection())
-            val items = mutableListOf<Track>()
-            while (cursor.moveToNext()) {
-                items.add(mapper.getTrack())
-            }
-            cursor.close()
-            items
-        }
-    }
+	override fun next(criteria: Criteria): Single<out List<Track>> {
+		return fromCallable {
+			val parser = CriteriaParser.from(criteria)
+			val uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI
+			val cursor = context.contentResolver.query(
+				uri,
+				parser.getProjection(),
+				parser.getSelection(),
+				parser.getSelectionArgs(),
+				parser.getSort()
+			)
+			val mapper = TrackMapper(cursor, parser.getProjection())
+			val items = mutableListOf<Track>()
+			while (cursor.moveToNext()) {
+				items.add(mapper.getTrack())
+			}
+			cursor.close()
+			items
+		}.withPool(pool)
+	}
 }
